e5182a1d8bcfe60ef26493801d676c702c33a4b3
Lydia Baik <lydiabaik@gmail.com>
2019-11-20 05:00:23 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,14 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  puts array.index(value_to_find)
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  puts array.sort.pop
+  array.sort.pop
 end
 
 def find_min_value(array)
-  # Add your solution here
+  puts array.sort.shift
+  array.sort.shift
 end
