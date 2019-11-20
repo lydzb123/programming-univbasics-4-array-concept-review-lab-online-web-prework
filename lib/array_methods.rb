065e45fb19358dfc2400f9c91263835bf9775014
Lydia Baik <lydiabaik@gmail.com>
2019-11-20 04:58:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
   puts array.index(value_to_find)
   array.index(value_to_find)
-
 end
 
 def find_max_value(array)
-  # Add your solution here
+  puts array.sort.pop
+  array.sort.pop
 end
 
 def find_min_value(array)
